a8e30acf52dd9df095ed53de57648a51aee6a7e3
JonathanMartin-gif <74462621+JonathanMartin-gif@users.noreply.github.com>
2021-01-14 10:15:26 -0600

    done with this small project

--- first-app/src/index.js
+++ first-app/src/index.js
@@ -3,17 +3,17 @@ import ReactDom from 'react-dom';
 
 
 const App = () => {
- const buttonText = {text: 'submit'};
- return(
+  const buttonText = {text: 'submit'};
+  return(
   <div>
     <label htmlFor='name' classnames='label'>enter e-main</label>
     <input id='name' type='text'/>
     <button style={{backgroundColor: 'red', color: 'white' }}>{buttonText.text}</button>
   </div>
- )
+  )
 }
 
 ReactDom.render(
- <App />,
- document.querySelector('#root')
+  <App />,
+  document.querySelector('#root')
 )
