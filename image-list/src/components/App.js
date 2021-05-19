42439c00b2f031955a0cabe4baca09714f3527f5
Jonathan <oldschoolspaceempire@gmail.com>
2021-05-19 11:09:22 -0500

    update

--- image-list/src/components/App.js
+++ image-list/src/components/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import SearchInput from './SearchInput';
-import axios from 'axios';
-import ImageList from './ImageList';
-
+import React from 'react'
+import SearchInput from './SearchInput'
+import axios from 'axios'
+import ImageList from './ImageList'
 
 class App extends React.Component {
- state = { images: []}
+  state = { images: [] }
 
-   onSearchSubmit = async (entry) => {
-    const response = await axios.get(`https://pixabay.com/api/?key=20010982-ade9eaec55e04821eeb5aa6a9&q=${entry}&image_type=photo&pretty=true`)
-    this.setState({images: response.data.hits})
- }
- render(){
-  return(
-   <div className='ui container' style={{marginTop:'30px'}}>
-    <SearchInput onSearchSubmit={this.onSearchSubmit}/>
-    <ImageList images={this.state.images}/>
-   </div>
-  )
- }
+  onSearchSubmit = async (entry) => {
+    const response = await axios.get(
+      `https://pixabay.com/api/?key=20010982-ade9eaec55e04821eeb5aa6a9&q=${entry}&image_type=photo&pretty=true`
+    )
+    this.setState({ images: response.data.hits })
+  }
+  render() {
+    return (
+      <div className='ui container' style={{ marginTop: '30px' }}>
+        <SearchInput onSearchSubmit={this.onSearchSubmit} />
+        <ImageList images={this.state.images} />
+      </div>
+    )
+  }
 }
 
-export default App;
-
-
+export default App
--- image-list/src/components/SearchInput.js
+++ image-list/src/components/SearchInput.js
@@ -1,36 +1,37 @@
-import React from 'react';
-
+import React from 'react'
 
 class SearchInput extends React.Component {
-
- // constructor() {
-//  super()
-//  this.onFormSubmit = this.onFormSubmit.bind(this) <---- bind this First way
-// }
- state = {entry: ''}
+  // constructor() {
+  //  super()
+  //  this.onFormSubmit = this.onFormSubmit.bind(this) <---- bind this First way
+  // }
+  state = { entry: '' }
 
   onFormSubmit = (event) => {
-  event.preventDefault()
-  this.props.onSearchSubmit(this.state.entry)
- }
+    event.preventDefault()
+    this.props.onSearchSubmit(this.state.entry)
+  }
   render() {
-   return(
-    <div className='ui segment'>
-     <form onSubmit={this.onFormSubmit} className='ui form'>
-      <div className='field'>
-       <div className="ui massive icon input">
-        <input type='text' 
-        placeholder='search...' 
-        onChange={(event) => this.setState({entry: event.target.value})}
-        value={this.state.entry}
-        />
-        <i className='search icon'></i>
-       </div>
+    return (
+      <div className='ui segment'>
+        <form onSubmit={this.onFormSubmit} className='ui form'>
+          <div className='field'>
+            <div className='ui massive icon input'>
+              <input
+                type='text'
+                placeholder='search...'
+                onChange={(event) =>
+                  this.setState({ entry: event.target.value })
+                }
+                value={this.state.entry}
+              />
+              <i className='search icon'></i>
+            </div>
+          </div>
+        </form>
       </div>
-     </form>
-    </div>
-   )
+    )
   }
 }
 
-export default SearchInput;
+export default SearchInput
