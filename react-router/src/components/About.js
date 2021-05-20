67e04c10bb1cf96dde924e92ee400d2c92ba73f1
Jonathan <oldschoolspaceempire@gmail.com>
2021-05-19 20:51:24 -0500

    added a model

--- react-router/src/components/About.js
+++ react-router/src/components/About.js
@@ -1,15 +1,26 @@
 import React from 'react'
+import './modal.css'
 
 const About = () => {
   return (
-    <div
-      class='ui raised very padded text container segment'
-      style={{ marginTop: '80px' }}
-    >
-      <h3 className='ui header'>About</h3>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque, earum?
-      </p>
+    <div>
+      <div className='wrapper'>
+        <div className='modal'>
+          <div className='modal-content'>
+            <h3>I am a modal</h3>
+          </div>
+        </div>
+      </div>
+      <div
+        class='ui raised very padded text container segment'
+        style={{ marginTop: '80px' }}
+      >
+        <h3 className='ui header'>About</h3>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque,
+          earum?
+        </p>
+      </div>
     </div>
   )
 }
