99976c80cdb33eb4c41f7d65a40b88a909ca4dc0
Jonathan <oldschoolspaceempire@gmail.com>
2021-05-21 15:57:03 -0500

    fixed a missing item

--- contextapi/src/components/Todolist.js
+++ contextapi/src/components/Todolist.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import { ThemeContext } from '../contexts/ThemeContext'
 
 class Todolist extends React.Component {
+  static contextType = ThemeContext
   render() {
+    const { isDarkTheme, lightTheme, darkTheme } = this.context
+    const theme = isDarkTheme ? darkTheme : lightTheme
     return (
       <div className='ui list'>
         <p className='item'>Plan the family trip</p>
